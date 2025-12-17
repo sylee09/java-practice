154c29e6e43b8b2d87dce10c43935f80eb117af9
Seungyoung <leesy960909@gmail.com>
2025-12-17 19:16:15 +0900

    타입 안정성을 위해 Team에서의 제네릭 타입을 Champion이나 Champion 하위타입으로 한정시킴

--- src/Team.java
+++ src/Team.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Team<T> {
+public class Team<T extends Champion> {
     private List<T> members = new ArrayList<>();
 
     List<T> getMembers() {
